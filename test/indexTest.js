43708ef4a76d397a20cb154d5e9361c685c17059
吴静波 <wujingbo@noahwm.com>
2016-06-24 18:41:25 +0800

    fix: fix test suite

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,9 @@ import Mengwang from '../src/index';
 
 describe('mengwang', () => {
   describe('init with default options', () => {
-    const instance = new Mengwang();
+    const instance = new Mengwang({
+      wsdl: 'abc'
+    });
     it('should set wsdl to default value', () => {
       expect(instance.options.debug).to.equal(Mengwang.defaultOptions.debug);
     });
